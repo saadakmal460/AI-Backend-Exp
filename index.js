4116dd18db05c0126405804b7b5366ace1fd1c43
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2024-11-08 18:58:50 +0500

    Changes

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose');
 const router = require('./Routes/Routes.js')
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 dotenv.config()
+app.use(cookieParser());
 app.use(cors())
 app.use(express.json())
 
