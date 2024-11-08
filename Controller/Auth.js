9a82763d6c75de64f0b86af0c84da2da6ec29a8c
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2024-11-08 18:45:51 +0500

    Cookie setting

--- Controller/Auth.js
+++ Controller/Auth.js
@@ -21,16 +21,26 @@ const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, 'User not found!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_TOKKEN);
+    
+    const token = jwt.sign({ id: validUser._id }, 'your_jwt_secret'); // You can hardcode the secret for now
+    
     const { password: pass, ...rest } = validUser._doc;
+
+    // Manual check for secure flag
+    const isProduction = true; // Manually set this to true or false based on your environment
     res
-      .cookie('access_token', token, { httpOnly: true })
+      .cookie('access_token', token, {
+        httpOnly: true,
+        secure: isProduction, // Will only be secure in production
+      })
       .status(200)
       .json(rest);
   } catch (error) {
     next(error);
   }
 };
+
+
 
 
 module.exports = {
